3880659583c963ac82c80a33c3f9e2ccd8ba036c
Antonello <antoiovi@antoiovi.com>
2024-07-23 15:33:02 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,28 @@
+import logging
+import logging.handlers
+import os
 
-import urllib.request
-import gzip
-import bs4
-from bs4 import BeautifulSoup
-import pandas as pd
-import re
-import datetime as dt
+import requests
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger_file_handler = logging.handlers.RotatingFileHandler(
+    "status.log",
+    maxBytes=1024 * 1024,
+    backupCount=1,
+    encoding="utf8",
+)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger_file_handler.setFormatter(formatter)
+logger.addHandler(logger_file_handler)
 
 
-
-
-print("CIAO")
+if __name__ == "__main__":
+    logger.info(f"Token value: {SOME_SECRET}")
+    url="https://www.borsaitaliana.it/borsa/derivati/indicatori-opzioni/open-interest.html"
+    r = requests.get(url)
+    if r.status_code == 200:
+        data = r.json()
+        print("STATUS 200")
+    else:
+      print("ERROR")
