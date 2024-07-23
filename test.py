2f4b83a54b01a01926f40bd1279f8d2b893903ef
Antonello <antoiovi@antoiovi.com>
2024-07-23 15:59:49 +0200

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,11 @@ import os
 
 import requests
 
+import urllib.request
+import gzip
+
+
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 logger_file_handler = logging.handlers.RotatingFileHandler(
@@ -19,9 +24,22 @@ logger.addHandler(logger_file_handler)
 
 if __name__ == "__main__":
     url="https://www.borsaitaliana.it/borsa/derivati/indicatori-opzioni/open-interest.html"
+    try:
+        request = urllib.request.Request(url)
+        request.add_header('Accept-encoding', 'gzip')
+        data = gzip.GzipFile(fileobj=urllib.request.urlopen(request)).read()
+        html = data.decode("utf-8")
+        logger.info(f'Html OK')
+        print("OK OK OK ")
+    except Exception as e:
+        logger.info(f'Exception raggiunta')
+        print(e)
+        
+    '''
     r = requests.get(url)
     if r.status_code == 200:
         data = r.json()
         print("STATUS 200")
     else:
       print("ERROR")
+    '''
